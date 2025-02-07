02114aa18b6aef019579a7be8a58d73dc581df70
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-07 16:07:47 +0100

    feat : geolocation for the interactive map

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
-const map = L.map('map').setView([46.519962, 6.633597], 12);
+let map;
 
-// Ajouter un fond de carte
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+function initMap() {
+  map = L.map('map').setView([0, 0], 2);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
-fetch('../world-heritage-list.json')
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((site) => {
-      const lat = site.coordinates.lat;
-      const lon = site.coordinates.lon;
-      const name = site.site;
-      L.marker([lat, lon]).addTo(map).bindPopup(name);
-    });
-  })
-  .catch((error) => console.error('Error loading or processing data:', error));
+  fetch('../world-heritage-list.json')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((site) => {
+        const lat = site.coordinates.lat;
+        const lon = site.coordinates.lon;
+        const name = site.site;
+        L.marker([lat, lon]).addTo(map).bindPopup(name);
+      });
+    })
+    .catch((error) =>
+      console.error('Error loading or processing data:', error)
+    );
+}
+
+navigator.geolocation.watchPosition((pos) => {
+  map.setView([pos.coords.latitude, pos.coords.longitude], 12);
+});
+
+initMap();
