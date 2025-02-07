b989e4296875aaa92c64d5bdfad47541644657d7
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-02-07 16:32:52 +0100

    [WIP]: chore(structure de base des dossiers et frameworks) : Ajout de la structure de base du code

---
+++ src/server.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+const app = express();
+
+// middlewares principaux
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('resources'));
+
+// Routes principales
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+// Démarrage du serveur
+https.createServer(app).listen(443, () => {
+    console.log('Server running on port 443');
+});
