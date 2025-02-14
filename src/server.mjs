2732c5482ca51a3bef3378384250a7f394366dff
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-14 15:27:53 +0100

    fix : ejs render, https certificate

--- src/server.mjs
+++ src/server.mjs
@@ -1,19 +1,29 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import https from "https";
+import https from 'https';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import path from 'path';
 const app = express();
 
 // middlewares principaux
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static("resources"));
+app.use(express.static('./resources'));
+
+app.set('view engine', 'ejs'); // Utilisation du moteur de rendu EJS
+app.set('views', path.resolve('src/views')); // Définition du dossier des vues
+
+const credentials = {
+  key: fs.readFileSync('./certificates/server.key'), // Clé privée
+  cert: fs.readFileSync('./certificates/server.crt'), // Certificat public
+};
 
 // Routes principales
-app.get("/", (req, res) => {
-  res.render("index.html");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 // Démarrage du serveur
-https.createServer(app).listen(443, () => {
-  console.log("Server running on port 443");
+https.createServer(credentials, app).listen(443, () => {
+  console.log('Server running on port 443 https://localhost:443');
 });
