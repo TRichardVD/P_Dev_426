d1c66d09d39ddaa29f7a64239e2746c229ece938
JerryCleuet <150446786+JerryCleuet@users.noreply.github.com>
2025-02-14 15:06:08 +0100

    fix()Importer https dans le fichier server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -1,18 +1,19 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cookieParser from "cookie-parser";
+import https from "https";
 const app = express();
 
 // middlewares principaux
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('resources'));
+app.use(express.static("resources"));
 
 // Routes principales
-app.get('/', (req, res) => {
-    res.render('index.html');
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
 // Démarrage du serveur
 https.createServer(app).listen(443, () => {
-    console.log('Server running on port 443');
+  console.log("Server running on port 443");
 });
