e6591b34a8e556e962c2692448ae65e629a87cbe
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-28 16:16:14 +0100

    feat : import all sites into our db

---
+++ src/helper/import.mjs
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import Site from '../models/site.mjs';
+import fs from 'fs';
+import path from 'path';
+
+export const importData = async () => {
+  const filePath = path.resolve('resources/world-heritage-list.json');
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const heritageSites = JSON.parse(data);
+  heritageSites.forEach((site) => {
+    const newSite = new Site({
+      name: site.site,
+      address: site.location,
+      description: site.short_description,
+    });
+    newSite.save();
+  });
+};
