1e1732882f3a71a9bcea7eea49affb21e911ce5b
NicolaGolaz <nicola.golaz@eduvaud.ch>
2025-03-21 16:30:08 +0100

    Ajout du footer et du header, css pas terminé

--- src/server.mjs
+++ src/server.mjs
@@ -1,33 +1,33 @@
-import https from 'https';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import fs from 'fs';
-import path from 'path';
-import connectDB from './db/mongoose.mjs';
-import { siteRouter } from './routes/sites.mjs';
+import https from "https";
+import express from "express";
+import cookieParser from "cookie-parser";
+import fs from "fs";
+import path from "path";
+import connectDB from "./db/mongoose.mjs";
+import { siteRouter } from "./routes/sites.mjs";
 const app = express();
 
 // middlewares principaux
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('./resources'));
+app.use(express.static("./resources"));
 
-app.set('view engine', 'ejs'); // Utilisation du moteur de rendu EJS
-app.set('views', path.resolve('src/views')); // Définition du dossier des vues
+app.set("view engine", "ejs"); // Utilisation du moteur de rendu EJS
+app.set("views", path.resolve("src/views")); // Définition du dossier des vues
 
 const credentials = {
-    key: fs.readFileSync('./certificates/server.key'), // Clé privée
-    cert: fs.readFileSync('./certificates/server.crt'), // Certificat public
+  key: fs.readFileSync("./certificates/server.key"), // Clé privée
+  cert: fs.readFileSync("./certificates/server.crt"), // Certificat public
 };
 
 // Routes principales
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.use('/api/site', siteRouter);
+app.use("/api/site", siteRouter);
 // Démarrage du serveur
 https.createServer(credentials, app).listen(443, () => {
-    connectDB();
-    console.log('Server running on port 443 https://localhost:443');
+  connectDB();
+  console.log("Server running on port 443 https://localhost:443");
 });
