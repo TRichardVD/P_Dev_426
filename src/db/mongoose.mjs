61826527f072252f23c41e2cd7135e4caf54788b
Emir Krasniqi <ph76act@eduvaud.ch>
2025-03-28 15:07:10 +0100

    Fix conficts

--- src/db/mongoose.mjs
+++ src/db/mongoose.mjs
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-import { importData } from '../helper/import.mjs';
+import mongoose from "mongoose";
+import { importData } from "../helper/import.mjs";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb://root:admin@localhost:27017/db_unesco?authSource=admin',
+      "mongodb://root:admin@localhost:27017/db_unesco?authSource=admin",
       {}
     );
     //importData();
-    console.log('Connexion réussie à MongoDB');
+    console.log("Connexion réussie à MongoDB");
   } catch (error) {
-    console.error('Erreur de connexion :', error);
+    console.error("Erreur de connexion :", error);
     process.exit(1); // Quitte le processus si la connexion échoue
   }
 };
