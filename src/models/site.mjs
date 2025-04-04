bbcfded9cbd33ebf90a0d81561a63f83242ed508
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-04 15:06:18 +0200

    refactor: delete useless indexes and add a language for the text research

--- src/models/site.mjs
+++ src/models/site.mjs
@@ -13,29 +13,33 @@ const CommentSchema = new Schema({
 });
 
 // Schéma pour le site
-const SiteSchema = new Schema({
-  name: { type: String, required: true },
-  address: { type: String },
-  description: { type: String },
-  country: [{ type: String }],
-  likes_count: { type: Number, default: 0 }, // Nombre de likes pour ce site
-  comments: [CommentSchema], // Liste des commentaires associés au site,
-  coordinates: {
-    type: {
-      type: String,
-      default: 'Point',
-      required: true,
-    },
+const SiteSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String },
+    description: { type: String },
+    country: [{ type: String }],
+    likes_count: { type: Number, default: 0 }, // Nombre de likes pour ce site
+    comments: [CommentSchema], // Liste des commentaires associés au site,
     coordinates: {
-      type: [Number],
-      required: true,
+      type: {
+        type: String,
+        default: 'Point',
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
   },
-});
-
-SiteSchema.index({ coordinates: '2dsphere' }); // Index pour la recherche géospatiale
+  { defaultLanguage: 'french' }
+);
 // Index pour la recherche textuelle
-SiteSchema.index({ name: 'text', description: 'text', country: 'text' });
+SiteSchema.index(
+  { name: 'text', description: 'text', country: 'text' },
+  { default_language: 'french' }
+);
 // Modèle Mongoose pour le site
 const Site = mongoose.model('Site', SiteSchema);
 
