1a73b85ffbe9f5c872046f496ebb0edc6d5106b7
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-04-11 16:10:00 +0200

    feat: add the lists to the model

--- src/models/user.mjs
+++ src/models/user.mjs
@@ -4,23 +4,30 @@ const { Schema, Types } = mongoose;
 
 // Schéma pour l'utilisateur
 const UserSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+  username: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+  },
+  likedSites: [{ type: Types.ObjectId, ref: 'Site', default: [] }], // Liste des sites aimés par l'utilisateur
+  comments: [
+    {
+      type: Types.ObjectId,
+      ref: 'Comment',
+      default: [],
     },
-    likedSites: [{ type: Types.ObjectId, ref: 'Site', default: [] }], // Liste des sites aimés par l'utilisateur
-    comments: [
-        {
-            type: Types.ObjectId,
-            ref: 'Comment',
-            default: [],
-        },
-    ],
-    sessions: [{ type: String, default: [] }], // Liste des sessions de l'utilisateur
-    password: { type: String, required: true, minlength: 6 }, // Le mot de passe avec une validation de longueur
+  ],
+  lists: [
+    {
+      name: { type: String },
+      color: { type: String },
+      sites: [{ type: Types.ObjectId, ref: 'Site', default: [] }],
+    },
+  ],
+  sessions: [{ type: String, default: [] }], // Liste des sessions de l'utilisateur
+  password: { type: String, required: true, minlength: 6 }, // Le mot de passe avec une validation de longueur
 });
 
 // Modèle Mongoose pour l'utilisateur
--- src/server.mjs
+++ src/server.mjs
@@ -1,14 +1,14 @@
-import https from "https";
-import express from "express";
-import cookieParser from "cookie-parser";
-import fs from "fs";
-import path from "path";
-import connectDB from "./db/mongoose.mjs";
-import { userRouter } from "./routes/user.mjs";
-import { siteRouter } from "./routes/sites.mjs";
-import { Register, Login, Logout } from "./controllers/auth.mjs";
-import { authReq } from "./controllers/auth.mjs";
-import { toggleLike, toggleDislike } from "./controllers/comments.mjs";
+import https from 'https';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import path from 'path';
+import connectDB from './db/mongoose.mjs';
+import { userRouter } from './routes/user.mjs';
+import { siteRouter } from './routes/sites.mjs';
+import { Register, Login, Logout } from './controllers/auth.mjs';
+import { authReq } from './controllers/auth.mjs';
+import { toggleLike, toggleDislike } from './controllers/comments.mjs';
 
 const app = express();
 
@@ -16,45 +16,45 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("./resources"));
+app.use(express.static('./resources'));
 
-app.set("view engine", "ejs"); // Utilisation du moteur de rendu EJS
-app.set("views", path.resolve("./src/views")); // Définition du dossier des vues
+app.set('view engine', 'ejs'); // Utilisation du moteur de rendu EJS
+app.set('views', path.resolve('./src/views')); // Définition du dossier des vues
 
 const credentials = {
-    key: fs.readFileSync("./certificates/server.key"), // Clé privée
-    cert: fs.readFileSync("./certificates/server.crt"), // Certificat public
+  key: fs.readFileSync('./certificates/server.key'), // Clé privée
+  cert: fs.readFileSync('./certificates/server.crt'), // Certificat public
 };
 
 // Routes d'affichages des pages d'accueil et de connexion
-app.get("/", (req, res) => {
-    res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/register", (req, res) => {
-    return res.render("auth/register", {
-        errors: {},
-    });
+app.get('/register', (req, res) => {
+  return res.render('auth/register', {
+    errors: {},
+  });
 });
-app.get("/login", (req, res) => {
-    return res.render("auth/login");
+app.get('/login', (req, res) => {
+  return res.render('auth/login');
 });
 
 // Routes API de l'authentification
-app.post("/register", Register);
-app.post("/login", Login);
-app.post("/logout", authReq, Logout);
+app.post('/register', Register);
+app.post('/login', Login);
+app.post('/logout', authReq, Logout);
 
 // Like/Dislike routes
-app.post("/api/comment/:id/like", authReq, toggleLike);
-app.post("/api/comment/:id/dislike", authReq, toggleDislike);
+app.post('/api/comment/:id/like', authReq, toggleLike);
+app.post('/api/comment/:id/dislike', authReq, toggleDislike);
 
 // Routes API de l'utilisateur et des sites
-app.use("/api/user", userRouter);
-app.use("/api/site", siteRouter);
+app.use('/api/user', userRouter);
+app.use('/api/site', siteRouter);
 
 // Démarrage du serveur
 https.createServer(credentials, app).listen(process.env.PORT || 443, () => {
-    connectDB();
-    console.log("Server running on port 443 https://localhost:443");
+  connectDB();
+  console.log('Server running on port 443 https://localhost:443');
 });
